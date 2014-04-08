efabdbdc6418fdded40765ea4ff6d43a1e9daacc
Changping Chen <ccp0101@gmail.com>
2014-04-08 02:11:11 -0400

    added rating storyboard

---
+++ FirstBase/RateGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  RateGameViewController.h
+//  FirstBase
+//
+//  Created by Changping Chen on 4/8/14.
+//  Copyright (c) 2014 Quan Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RateGameViewController : UITableViewController
+
+@end
